a3adaf6d9451dc862874245abc5a1a9576e23475
josephriosio <blankworldwide@gmail.com>
2019-03-19 20:40:19 -0700

    added bootstrap, created basic contact and header components

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+
+const members = [
+  {
+    name: "Joseph Rios",
+    email: "josephrios56@gmail.com",
+    phone: "925-325-6695"
+  },
+  {
+    name: "marysol Rios",
+    email: "marysolrios56@gmail.com",
+    phone: "925-729-6695"
+  }
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>hello world!</h1>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          {members.map(member => (
+            <Contact membersProps={member} />
+          ))}
+        </div>
       </div>
     );
   }
