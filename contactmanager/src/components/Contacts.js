580cdf1a9db89191c8257a50fe835356176bc4b3
josephriosio <blankworldwide@gmail.com>
2019-03-28 13:52:05 -0700

    refactored code to include Consumer and Provider

--- contactmanager/src/components/Contacts.js
+++ contactmanager/src/components/Contacts.js
@@ -1,58 +1,22 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "Joseph Rios",
-        email: "josephrios56@gmail.com",
-        phone: "925-325-6695"
-      },
-      {
-        id: 2,
-        name: "marysol Rios",
-        email: "marysolrios56@gmail.com",
-        phone: "925-729-6695"
-      },
-      {
-        id: 3,
-        name: "jon smith",
-        email: "jonsmith@gmail.com",
-        phone: "650-782-6646"
-      },
-      {
-        id: 4,
-        name: "jon notsmith",
-        email: "jonsmith@gmail.com",
-        phone: "650-782-6646"
-      }
-    ]
-  };
-
-  deleteContact = id => {
-    const { contacts } = this.state;
-
-    const newContacts = contacts.filter(contact => contact.id !== id);
-
-    this.setState({
-      contacts: newContacts
-    });
-  };
-
   render() {
-    const { contacts } = this.state;
     return (
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
---
+++ contactmanager/src/context.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "Joseph Rios",
+        email: "josephrios56@gmail.com",
+        phone: "925-325-6695"
+      },
+      {
+        id: 2,
+        name: "marysol Rios",
+        email: "marysolrios56@gmail.com",
+        phone: "925-729-6695"
+      },
+      {
+        id: 3,
+        name: "jon smith",
+        email: "jonsmith@gmail.com",
+        phone: "650-782-6646"
+      },
+      {
+        id: 4,
+        name: "jon notsmith",
+        email: "jonsmith@gmail.com",
+        phone: "650-782-6646"
+      }
+    ],
+    dispatch: action => this.setState(state => reducer(state, action))
+  };
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
