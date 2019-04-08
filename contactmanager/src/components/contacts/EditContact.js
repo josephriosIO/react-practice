c8a080b84bb7ed5bb6caf515e934e5a28d2b50a5
josephriosio <blankworldwide@gmail.com>
2019-04-07 21:57:46 -0700

    added update with axios

--- contactmanager/src/components/contacts/EditContact.js
+++ contactmanager/src/components/contacts/EditContact.js
@@ -50,6 +50,21 @@ class EditContact extends Component {
       return;
     }
 
+    const updContact = {
+      name,
+      email,
+      phone
+    };
+
+    const { id } = this.props.match.params;
+
+    const res = await axios.put(
+      `https://jsonplaceholder.typicode.com/users/${id}`,
+      updContact
+    );
+
+    dispatch({ type: "UPDATE_CONTACT", payload: res.data });
+
     this.setState({
       name: "",
       phone: "",
